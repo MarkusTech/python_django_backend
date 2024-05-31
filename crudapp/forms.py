287c53ad53b11386dac3e341c3e190ed7c784350
MarkusTech <wm.recopelacion@gmail.com>
2024-05-31 14:30:17 +0800

    first commit

---
+++ crudapp/forms.py
@@ -0,0 +1,9 @@
+# crudapp/forms.py
+
+from django import forms
+from .models import Item
+
+class ItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ['name', 'description', 'price']
